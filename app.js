6d228d41cbb50fcd3b656325c73eac4312bb07a1
Mouha Diouf <mouhamadoudioufsn@gmail.com>
2021-03-14 09:45:03 +0000

    Take input from terminal prompt to show weather data

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
-
-geocode("Dakar", (error, data) => {
+geocode(process.argv[2], (error, data) => {
+  if (!process.argv[2]) {
+    return console.log("Please provide an address!!");
+  }
   if (error) {
     console.log(error);
     return;
   }
   console.log("Error", error);
   console.log("Data", data);
-  forecast(data.latitude, data.longitude, (error, data) => {
-    console.log("Error", error);
-    console.log("Data", data);
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log(`${data.location}`);
+    console.log(forecastData);
   });
 });
